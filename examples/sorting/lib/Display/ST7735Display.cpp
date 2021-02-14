2b62d1000d37073fb0406d7c06bf4f4f39da5cd3
Luke de Munk <luke.de.munk@hva.nl>
2021-02-14 23:25:38 +0100

    Added display library

---
+++ examples/sorting/lib/Display/ST7735Display.cpp
@@ -0,0 +1,51 @@
+/*
+ * File:      TestUtility.h
+ * Authors:   Luke de Munk
+ * Class:     TestUtility
+ * Version:   1.0
+ *
+ * Test utility test arrays.
+*/
+#include "./ST7735Display.h"
+
+
+ST7735Display::ST7735Display() {
+  _tft = Adafruit_ST7735(CS_PIN, DC_PIN, RST_PIN);
+  _color = 0xFF;
+
+  _tft.initR(INITR_MINI160x80);                                             //Init ST7735S mini display
+  
+  /* SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
+    Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
+    may end up with a black screen some times, or all the time. */
+  //_tft.setSPISpeed(40000000);
+  
+  _tft.setColRowStart(26, 1);                                           //For my specific display I had to adjust it
+  _tft.setRotation(3);
+  _tft.invertDisplay(true);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Shows a list of elements on a display.
+    @param  list    Array of elements to be sorted
+    @param  n       Length of the array
+*/
+/**************************************************************************/
+void ST7735Display::showList(int32_t list[], uint32_t n) {
+    uint16_t height = 0;
+
+    if (n > SCREEN_WIDTH)
+        n = SCREEN_WIDTH;
+    for (uint16_t x = 0; x < n; x++) {
+        if (list[x] > SCREEN_HEIGHT) {
+            height = SCREEN_HEIGHT;
+        } else {
+            height = list[x];
+        }
+        for (uint16_t y = 0; y < height; y++) {
+            _tft.drawPixel(x, y, _color);
+        }
+    }
+}
+#endif
---
+++ examples/sorting/lib/Display/ST7735Display.h
@@ -0,0 +1,36 @@
+/*
+ * File:      ST7735Display.h
+ * Authors:   Luke de Munk
+ * Class:     ST7735Display
+ * Version:   1.0
+ *
+ * Test utility test arrays.
+*/
+#ifndef ST7735Display_H
+#define ST7735Display_H
+#include <Adafruit_GFX.h>                       // Core graphics library
+#include <Adafruit_ST7735.h>                    // Hardware-specific library for ST7735
+#include <SPI.h>
+
+#define SCLK_PIN        18        //SPI clock (D18) Don't change
+#define SDA_PIN         23        //SPI data (D23) Don't change
+#define DC_PIN          2         //Register select (stands for Data Control perhaps!) (0 = command, 1 = display) (D2)
+#define CS_PIN          5         //Display enable (Chip select), if not enabled will not talk on SPI bus (D5)
+
+#define RST_PIN         15        //Display reset pin (D15)
+#define BLK_PIN         4         //Backlight control pin (0 = off, 1 = on) (D4)
+
+#define SCREEN_WIDTH    160
+#define SCREEN_HEIGHT   80
+
+class ST7735Display
+{
+  public:
+    ST7735Display();
+    void showList(int32_t list[], uint32_t n);
+  
+  private:
+    Adafruit_ST7735 _tft;
+    uint16_t _color;
+};
+#endif
